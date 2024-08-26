c453a542863761811d8abb88cc449a4ca1f18fdf
assem <gddfggggdadg12@gmail.com>
2024-08-26 16:07:28 +0300

    change photo

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -25,259 +25,259 @@ import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 import { useLocation, useNavigate } from "react-router-dom";
 import { grey } from "@mui/material/colors";
 // @ts-ignore
-import userImg from "../images/user-img.jpg";
+import userImg from "../images/a.jpg";
 
 const drawerWidth = 240;
 const openedMixin = (theme) => ({
-	width: drawerWidth,
-	transition: theme.transitions.create("width", {
-		easing: theme.transitions.easing.sharp,
-		duration: theme.transitions.duration.enteringScreen,
-	}),
-	overflowX: "hidden",
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-	transition: theme.transitions.create("width", {
-		easing: theme.transitions.easing.sharp,
-		duration: theme.transitions.duration.leavingScreen,
-	}),
-	overflowX: "hidden",
-	width: `calc(${theme.spacing(7)} + 1px)`,
-	[theme.breakpoints.up("sm")]: {
-		width: `calc(${theme.spacing(8)} + 1px)`,
-	},
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
 });
 
 const Drawer = styled(MuiDrawer, {
-	shouldForwardProp: (prop) => prop !== "open",
-	// @ts-ignore
+  shouldForwardProp: (prop) => prop !== "open",
+  // @ts-ignore
 })(({ theme, open }) => ({
-	width: drawerWidth,
-	flexShrink: 0,
-	whiteSpace: "nowrap",
-	boxSizing: "border-box",
-	...(open && {
-		...openedMixin(theme),
-		"& .MuiDrawer-paper": openedMixin(theme),
-	}),
-	...(!open && {
-		...closedMixin(theme),
-		"& .MuiDrawer-paper": closedMixin(theme),
-	}),
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
 }));
 
 const DrawerHeader = styled("div")(({ theme }) => ({
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "flex-end",
-	padding: theme.spacing(0, 1),
-	// necessary for content to be below app bar
-	...theme.mixins.toolbar,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
 }));
 
 const Array1 = [
-	{ text: "Dashboard", icon: <HomeOutlinedIcon />, path: "/" },
-	{ text: "Manage Team", icon: <PeopleOutlinedIcon />, path: "/team" },
-	{
-		text: "Contacts Information",
-		icon: <ContactsOutlinedIcon />,
-		path: "/contacts",
-	},
-	{
-		text: "Invoices Balances",
-		icon: <ReceiptOutlinedIcon />,
-		path: "/invoices",
-	},
+  { text: "Dashboard", icon: <HomeOutlinedIcon />, path: "/" },
+  { text: "Manage Team", icon: <PeopleOutlinedIcon />, path: "/team" },
+  {
+    text: "Contacts Information",
+    icon: <ContactsOutlinedIcon />,
+    path: "/contacts",
+  },
+  {
+    text: "Invoices Balances",
+    icon: <ReceiptOutlinedIcon />,
+    path: "/invoices",
+  },
 ];
 
 const Array2 = [
-	{ text: "Profile Form", icon: <PersonOutlinedIcon />, path: "/form" },
-	{ text: "Calendar", icon: <CalendarTodayOutlinedIcon />, path: "/calendar" },
-	{
-		text: "FAQ Page",
-		icon: <HelpOutlineOutlinedIcon />,
-		path: "/faq",
-	},
+  { text: "Profile Form", icon: <PersonOutlinedIcon />, path: "/form" },
+  { text: "Calendar", icon: <CalendarTodayOutlinedIcon />, path: "/calendar" },
+  {
+    text: "FAQ Page",
+    icon: <HelpOutlineOutlinedIcon />,
+    path: "/faq",
+  },
 ];
 
 const Array3 = [
-	{ text: "Bar Chart", icon: <BarChartOutlinedIcon />, path: "/bar" },
-	{ text: "Pie Chart", icon: <PieChartOutlineOutlinedIcon />, path: "/pie" },
-	{ text: "Line Chart", icon: <TimelineOutlinedIcon />, path: "/line" },
-	{ text: "Geography Chart", icon: <MapOutlinedIcon />, path: "/geography" },
+  { text: "Bar Chart", icon: <BarChartOutlinedIcon />, path: "/bar" },
+  { text: "Pie Chart", icon: <PieChartOutlineOutlinedIcon />, path: "/pie" },
+  { text: "Line Chart", icon: <TimelineOutlinedIcon />, path: "/line" },
+  { text: "Geography Chart", icon: <MapOutlinedIcon />, path: "/geography" },
 ];
 
 // eslint-disable-next-line react/prop-types
 const SideBar = ({ open, handleDrawerClose }) => {
-	let location = useLocation();
-	const navigate = useNavigate();
-	const theme = useTheme();
-	return (
-		<Drawer variant='permanent' open={open}>
-			<DrawerHeader>
-				<IconButton onClick={handleDrawerClose}>
-					{theme.direction === "rtl" ? (
-						<ChevronRightIcon />
-					) : (
-						<ChevronLeftIcon />
-					)}
-				</IconButton>
-			</DrawerHeader>
-			<Divider />
-			<Avatar
-				sx={{
-					mx: "auto",
-					width: open ? 88 : 44,
-					height: open ? 88 : 44,
-					my: 1,
-					border: `2px solid ${theme.palette.info.main}`,
-					transition: "0.25s",
-				}}
-				alt='Abdelazim Hassan'
-				src={userImg}
-			/>
-			<Typography
-				align='center'
-				sx={{ fontSize: open ? 20 : 0, transition: "0.25s" }}
-			>
-				Abdelazim Hassan
-			</Typography>
-			<Typography
-				align='center'
-				sx={{
-					fontSize: open ? 16 : 0,
-					transition: "0.25s",
-					color: theme.palette.info.main,
-				}}
-			>
-				Admin
-			</Typography>
+  let location = useLocation();
+  const navigate = useNavigate();
+  const theme = useTheme();
+  return (
+    <Drawer variant="permanent" open={open}>
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === "rtl" ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <Avatar
+        sx={{
+          mx: "auto",
+          width: open ? 88 : 44,
+          height: open ? 88 : 44,
+          my: 1,
+          border: `2px solid ${theme.palette.info.main}`,
+          transition: "0.25s",
+        }}
+        alt="Assem Mohamed"
+        src={userImg}
+      />
+      <Typography
+        align="center"
+        sx={{ fontSize: open ? 20 : 0, transition: "0.25s" }}
+      >
+        Assem Mohamed
+      </Typography>
+      <Typography
+        align="center"
+        sx={{
+          fontSize: open ? 16 : 0,
+          transition: "0.25s",
+          color: theme.palette.info.main,
+        }}
+      >
+        Admin
+      </Typography>
 
-			<Divider />
+      <Divider />
 
-			<List>
-				{Array1.map((item) => (
-					<ListItem key={item.path} disablePadding sx={{ display: "block" }}>
-						<Tooltip title={open ? null : item.text} placement='left' arrow>
-							<ListItemButton
-								onClick={() => {
-									navigate(item.path);
-								}}
-								sx={{
-									minHeight: 48,
-									justifyContent: open ? "initial" : "center",
-									px: 2.5,
-									bgcolor:
-										location.pathname === item.path
-											? theme.palette.mode === "dark"
-												? grey[800]
-												: grey[200]
-											: null,
-								}}
-							>
-								<ListItemIcon
-									sx={{
-										minWidth: 0,
-										mr: open ? 3 : "auto",
-										justifyContent: "center",
-									}}
-								>
-									{item.icon}
-								</ListItemIcon>
-								<ListItemText
-									primary={item.text}
-									sx={{ opacity: open ? 1 : 0 }}
-								/>
-							</ListItemButton>
-						</Tooltip>
-					</ListItem>
-				))}
-			</List>
+      <List>
+        {Array1.map((item) => (
+          <ListItem key={item.path} disablePadding sx={{ display: "block" }}>
+            <Tooltip title={open ? null : item.text} placement="left" arrow>
+              <ListItemButton
+                onClick={() => {
+                  navigate(item.path);
+                }}
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                  bgcolor:
+                    location.pathname === item.path
+                      ? theme.palette.mode === "dark"
+                        ? grey[800]
+                        : grey[200]
+                      : null,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText
+                  primary={item.text}
+                  sx={{ opacity: open ? 1 : 0 }}
+                />
+              </ListItemButton>
+            </Tooltip>
+          </ListItem>
+        ))}
+      </List>
 
-			<Divider />
+      <Divider />
 
-			<List>
-				{Array2.map((item) => (
-					<ListItem key={item.path} disablePadding sx={{ display: "block" }}>
-						<Tooltip title={open ? null : item.text} placement='left' arrow>
-							<ListItemButton
-								onClick={() => {
-									navigate(item.path);
-								}}
-								sx={{
-									minHeight: 48,
-									justifyContent: open ? "initial" : "center",
-									px: 2.5,
-									bgcolor:
-										location.pathname === item.path
-											? theme.palette.mode === "dark"
-												? grey[800]
-												: grey[200]
-											: null,
-								}}
-							>
-								<ListItemIcon
-									sx={{
-										minWidth: 0,
-										mr: open ? 3 : "auto",
-										justifyContent: "center",
-									}}
-								>
-									{item.icon}
-								</ListItemIcon>
-								<ListItemText
-									primary={item.text}
-									sx={{ opacity: open ? 1 : 0 }}
-								/>
-							</ListItemButton>
-						</Tooltip>
-					</ListItem>
-				))}
-			</List>
+      <List>
+        {Array2.map((item) => (
+          <ListItem key={item.path} disablePadding sx={{ display: "block" }}>
+            <Tooltip title={open ? null : item.text} placement="left" arrow>
+              <ListItemButton
+                onClick={() => {
+                  navigate(item.path);
+                }}
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                  bgcolor:
+                    location.pathname === item.path
+                      ? theme.palette.mode === "dark"
+                        ? grey[800]
+                        : grey[200]
+                      : null,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText
+                  primary={item.text}
+                  sx={{ opacity: open ? 1 : 0 }}
+                />
+              </ListItemButton>
+            </Tooltip>
+          </ListItem>
+        ))}
+      </List>
 
-			<Divider />
+      <Divider />
 
-			<List>
-				{Array3.map((item) => (
-					<ListItem key={item.path} disablePadding sx={{ display: "block" }}>
-						<Tooltip title={open ? null : item.text} placement='left' arrow>
-							<ListItemButton
-								onClick={() => {
-									navigate(item.path);
-								}}
-								sx={{
-									minHeight: 48,
-									justifyContent: open ? "initial" : "center",
-									px: 2.5,
-									bgcolor:
-										location.pathname === item.path
-											? theme.palette.mode === "dark"
-												? grey[800]
-												: grey[200]
-											: null,
-								}}
-							>
-								<ListItemIcon
-									sx={{
-										minWidth: 0,
-										mr: open ? 3 : "auto",
-										justifyContent: "center",
-									}}
-								>
-									{item.icon}
-								</ListItemIcon>
-								<ListItemText
-									primary={item.text}
-									sx={{ opacity: open ? 1 : 0 }}
-								/>
-							</ListItemButton>
-						</Tooltip>
-					</ListItem>
-				))}
-			</List>
-		</Drawer>
-	);
+      <List>
+        {Array3.map((item) => (
+          <ListItem key={item.path} disablePadding sx={{ display: "block" }}>
+            <Tooltip title={open ? null : item.text} placement="left" arrow>
+              <ListItemButton
+                onClick={() => {
+                  navigate(item.path);
+                }}
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                  bgcolor:
+                    location.pathname === item.path
+                      ? theme.palette.mode === "dark"
+                        ? grey[800]
+                        : grey[200]
+                      : null,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText
+                  primary={item.text}
+                  sx={{ opacity: open ? 1 : 0 }}
+                />
+              </ListItemButton>
+            </Tooltip>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
 };
 
 export default SideBar;
